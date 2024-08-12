6ee66618f6d7139285d82cc1490bacf230c5ca42
homer <homer@simpson.simpson>
2024-08-12 03:31:14 -0400

    Ready for the Jamgit add .git add .!

--- Scripts/level_four.gd
+++ Scripts/level_four.gd
@@ -9,6 +9,10 @@ func _on_child_exiting_tree(node):
 	if node.name == "PouncerTwo":
 		$GameOverMenu.show()
 
-
+#NOTE: NOT CONNECTED
 func _on_hide_box_inside_box():
 	$PouncerTwo.hide_kitty == true
+
+
+func _on_audio_stream_player_2d_finished():
+	$AudioStreamPlayer2D.play()
